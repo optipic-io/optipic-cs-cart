28486d84bbb06e3c56f4ddb2aab31b296ff9740a
optipic <info@optipic.io>
2021-05-05 05:17:30 +0300

    v1.14.0

---
+++ app/addons/optipic/Optipic.php
@@ -0,0 +1,30 @@
+<?php
+use Tygh\Registry;
+use \optipic\cdn\ImgUrlConverter;
+
+class Optipic
+{
+    public function getSettings()
+    {
+        return array(
+            'autoreplace_active' => Registry::get('addons.optipic.autoreplace_active'),
+            'site_id' => Registry::get('addons.optipic.site_id'),
+            'domains' => Registry::get('addons.optipic.domains')!='' ? explode("\n", Registry::get('addons.optipic.domains')) : array(),
+            'exclusions_url' => Registry::get('addons.optipic.exclusions_url')!='' ? explode("\n", Registry::get('addons.optipic.exclusions_url')) : array(),
+            'whitelist_img_urls' => Registry::get('addons.optipic.whitelist_img_urls')!='' ? explode("\n", Registry::get('addons.optipic.whitelist_img_urls')) : array(),
+            'srcset_attrs' => Registry::get('addons.optipic.srcset_attrs')!='' ? explode("\n", Registry::get('addons.optipic.srcset_attrs')) : array(),
+        );
+    }
+
+    public function changeContent($content)
+    {
+        $settings = $this->getSettings();
+
+        if ($settings['autoreplace_active']=='Y' && $settings['site_id']!=''){
+            ImgUrlConverter::loadConfig($settings);
+            $content = ImgUrlConverter::convertHtml($content);
+        }
+
+        return $content;
+    }
+}
--- app/addons/optipic/init.php
+++ app/addons/optipic/init.php
@@ -1,8 +1,10 @@
 <?php
-
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
+require_once dirname(__FILE__) . '/ImgUrlConverter.php';
+require_once dirname(__FILE__) . '/Optipic.php';
+
 fn_register_hooks(
-    'complete',
-    'dispatch_before_send_response'
+    ['dispatch_before_send_response', PHP_INT_MAX],
+    ['complete', PHP_INT_MIN]
 );
